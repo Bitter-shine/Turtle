7f082abac3f35bde0d70d1f3c24a31c6a465d761
Bitter-shine <77920708+Bitter-shine@users.noreply.github.com>
2021-02-27 14:12:42 +0800

    Update Animal.py
    I change file to English,its easy to see,try it!

--- Animal.py
+++ Animal.py
@@ -3,30 +3,37 @@ class Animal():
     def seeanimal(self):
         n = random.randint(1,5)
         if n == 1:
-            m = "吃饭"
+            m = "eating🍔"
         elif n == 2:
-            m = "喝奶"
+            m = "drinking"
         elif n == 3:
-            m = "玩耍"
+            m = "playing"
         elif n == 4:
-            m = "睡觉"
+            m = "sleeping😪"
         else:
-            m = "运动"
-        print(f"{name}正在{m}")
+            m = "is excreting💩"
+        print(f"{name} is {m}")
     def animalmood(self):
         mood = random.randint(1, 5)
         if mood == 1:
-            m = "很高兴"
+            m = " is happy😄"
         elif mood == 2:
-            m = "愁眉苦脸"
+            m = " is bad😞"
         elif mood == 3:
-            m = "开怀大笑"
+            m = " is tired"
         elif mood == 4:
-            m = "很疑惑"
+            m = " is doubt"
         else:
-            m = "很失望"
+            m = " is angry😠"
         print(f"{name}{m}")
 a = Animal()
+print("******** Animal Home ********")
+print("1.Use a.see to look animal's doing.")
+print("2.Use a.name to change your animal's name.")
+print("3.Use a.mood to see animal's mood.")
+print("4.Use quit end the file.")
+name = input("Your animal name:")
+Animal()
 while True:
     i = input(str(">>>"))
     if i == "a.see":
